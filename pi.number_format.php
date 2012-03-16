824aeb2ce1a9ca0e860e8a5fdc35ed1f2d39aec1
Robert Wallis <robert@verbalink.com>
2012-03-16 13:22:26 -0700

    Guillaume Desjonqueres @ Hambo Development fixed issue with float vals in a string 0.00.  Thanks Guillamue.

--- pi.number_format.php
+++ pi.number_format.php
@@ -8,30 +8,33 @@ class number_format
 	function number_format($direct='')
 	{
 		$this->EE =& get_instance();
-		
+
 		// support all three ways of passing a number
 		$number = 0.0;
-		if (!empty($direct) && 0.0 != ($number=floatval($direct))) {
+		if (!empty($direct) && 0.0 != floatval($direct)) {
 			// data directly from Publish
 			// $number set in if clause
+			$number = floatval($direct);
 		} else if (
-			NULL != ($number=$this->EE->TMPL->fetch_param('number', NULL)) &&
-			0.0 != floatval($number)
+			NULL !== $this->EE->TMPL->fetch_param('number', NULL) &&
+			0.0 != floatval($this->EE->TMPL->fetch_param('number', NULL))
 		) {
 			// data directly from the "number" paramater
 			// $number set in if clause;
+			$number = $this->EE->TMPL->fetch_param('number', NULL);
 		} else if (
-			'' != ($number=$this->EE->TMPL->tagdata) &&
-			0.0 != floatval($number)
+			'' != $this->EE->TMPL->tagdata &&
+			0.0 != floatval($this->EE->TMPL->tagdata)
 		) {
 			// data from inside the tag contents
 			// $number set in if clause;
+			$number = $this->EE->TMPL->tagdata;
 		}
-		
+
 		$decimals = $this->EE->TMPL->fetch_param('decimals', NULL);
 		$dec_point = $this->EE->TMPL->fetch_param('dec_point', NULL);
 		$thousands_sep = $this->EE->TMPL->fetch_param('thousands_sep', NULL);
-		
-		$this->return_data = number_format($number, $decimals, $dec_point, $thousands_sep);
+
+		$this->return_data = number_format('', $decimals, $dec_point, $thousands_sep);
 	}
 }
